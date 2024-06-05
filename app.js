477dbb17424d9f37ea31856e7f29d1fd5bd17e82
YourFather <igorprolazov@gmail.com>
2024-06-06 02:11:51 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+/*if (numOrStr === null) {
+  console.log("ви скасували");
+} else if (numOrStr.trim() === "") {
+  console.log("Empty String");
+} else if (isNaN(+numOrStr)) {
+  console.log(" number is Ba_NaN");
+} else {
+  console.log("OK!");
+}*/
+
+let numOrStr = prompt("input number or string");
+console.log(numOrStr);
+
+switch (true) {
+  case numOrStr === null:
+    console.log("ви скасували");
+    break;
+  case numOrStr.trim() === ``:
+    console.log("Empty String");
+    break;
+  case isNaN(+numOrStr):
+    console.log(" number is Ba_NaN");
+    break;
+  default:
+    console.log("OK!");
+}
